3b05f561ff67330a0be8ea1994db3be37f016ad4
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-23 18:54:07 +0100

    Aggiunto milestone 3

--- js/script.js
+++ js/script.js
@@ -118,6 +118,8 @@ const icons = [
   },
 ];
 
+
+
 // Milestone 1
 
 icons.forEach((element, array) => {
@@ -131,24 +133,117 @@ icons.forEach((element, array) => {
 
 // Milestone 2
 
-var iconeFontawesome = document.getElementsByTagName("i");
-for(var i = 0; i < iconeFontawesome.length; i++){
-
-   if (iconeFontawesome[i].classList.contains("food")) {
-     $(iconeFontawesome[i]).addClass("green");
-   } else if (iconeFontawesome[i].classList.contains("beverage")) {
-     $(iconeFontawesome[i]).addClass("blue");
-   } else {
-     $(iconeFontawesome[i]).addClass("orange");
-   }
-
-}
+// var iconeFontawesome = document.getElementsByTagName("i");
+// for(var i = 0; i < iconeFontawesome.length; i++){
+//
+//    if (iconeFontawesome[i].classList.contains("food")) {
+//      $(iconeFontawesome[i]).addClass("green");
+//    } else if (iconeFontawesome[i].classList.contains("beverage")) {
+//      $(iconeFontawesome[i]).addClass("blue");
+//    } else {
+//      $(iconeFontawesome[i]).addClass("orange");
+//    }
+//
+// }
 
 
 // Milestone 3
 
+var filteredIcons = [];
+
+$('select').change(function(){
+
+  $(".icons").empty();
+
+  if (this.value == "food") {
+    filteredIcons = icons.filter((element) => {
+      return element.category =="food";
+    })
+  } else if (this.value == "beverage") {
+    filteredIcons = icons.filter((element) => {
+      return element.category =="beverage";
+    })
+  } else if (this.value == "animal") {
+    filteredIcons = icons.filter((element) => {
+      return element.category =="animal";
+    })
+  } else {
+    filteredIcons = icons.map((element) => {
+      return element;
+    })
+  }
+
+  filteredIcons.forEach((element, array) => {
+    let icona = `<div>
+                  <i class="${element.family} ${element.prefix}${element.name} ${element.category}"></i>
+                  <div class="title">${element.name}</div>
+                </div>`;
+    $('.icons').append(icona);
+  });
 
 
 
 
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Alternativa per Milestone 1 / 2
+
+// const food = icons.filter((element) => {
+//   return element.category == "food";
+// });
+// const beverage = icons.filter((element) => {
+//   return element.category == "beverage";
+// });
+// const animal = icons.filter((element) => {
+//   return element.category == "animal";
+// });
 //
+// let icona;
+// // Icone Food
+// let iconeFood = "";
+// food.forEach((element, array) => {
+//   icona = `<div>
+//             <i class="${element.family} ${element.prefix}${element.name} green ${element.category}"></i>
+//             <div class="title">${element.name}</div>
+//           </div>`;
+//   iconeFood += icona;
+// });
+// $('.icons').append(iconeFood);
+//
+// // Icone Beverage
+// let iconeBeverage = "";
+// beverage.forEach((element, array) => {
+//   icona = `<div>
+//             <i class="${element.family} ${element.prefix}${element.name} blue ${element.category}"></i>
+//             <div class="title">${element.name}</div>
+//           </div>`;
+//   iconeBeverage += icona;
+// });
+// $('.icons').append(iconeBeverage);
+//
+// // Icone Animal
+// let iconeAnimal = "";
+// animal.forEach((element, array) => {
+//   icona = `<div>
+//             <i class="${element.family} ${element.prefix}${element.name} orange ${element.category}"></i>
+//             <div class="title">${element.name}</div>
+//           </div>`;
+//   iconeAnimal += icona;
+// });
+// $('.icons').append(iconeAnimal);
