39552be7a6d4cc215a8753a4e248dad4081b8958
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-23 21:46:23 +0100

    Aggiunto milestone 2 usando es6

--- js/script.js
+++ js/script.js
@@ -45,6 +45,42 @@ const icons = [
     category: "food"
   },
   {
+    name: 'bacon',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'bone',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'bread-slice',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'candy-cane',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'carrot',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'cheese',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
     name: 'beer',
     family: 'fas',
     prefix: 'fa-',
@@ -75,10 +111,75 @@ const icons = [
     category: "beverage"
   },
   {
-    name: 'glass-martini',
+    name: 'blender',
     family: 'fas',
     prefix: 'fa-',
     category: "beverage"
+  },
+  {  name: 'flask',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-cheers',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-martini-alt',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-whiskey',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'coffee',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'mug-hot',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'wine-glass-alt',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'cat',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'crow',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'dog',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'dove',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
   },
   {
     name: 'dragon',
@@ -87,7 +188,19 @@ const icons = [
     category: "animal"
   },
   {
-    name: 'kiwi-bird',
+    name: 'feather',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'feather-alt',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'fish',
     family: 'fas',
     prefix: 'fa-',
     category: "animal"
@@ -105,34 +218,88 @@ const icons = [
     category: "animal"
   },
   {
+    name: 'horse',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'horse-head',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'kiwi-bird',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
     name: 'otter',
     family: 'fas',
     prefix: 'fa-',
     category: "animal"
   },
   {
-    name: 'horse',
+    name: 'paw',
     family: 'fas',
     prefix: 'fa-',
     category: "animal"
   },
+  {
+    name: 'spider',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  }
 ];
 
 
 
 // Milestone 1
 
-icons.forEach((element, array) => {
-  let icona = `<div>
-                <i class="${element.family} ${element.prefix}${element.name} ${element.category}"></i>
-                <div class="title">${element.name}</div>
-              </div>`;
-  $('.icons').append(icona);
-});
+// icons.forEach((element, array) => {
+//   let icona = `<div>
+//                 <i class="${element.family} ${element.prefix}${element.name} ${element.category}"></i>
+//                 <div class="title">${element.name}</div>
+//               </div>`;
+//   $('.icons').append(icona);
+// });
 
 
 // Milestone 2
 
+const colors = ["red", "green", "blue"];
+const categories = [];
+
+icons.forEach((item) => {
+  const categoria = item.category;
+  if (categories.includes(categoria) == false) {
+    categories.push(categoria);
+  }
+});
+
+for (var i = 0; i < categories.length; i++) {
+
+  icons.forEach((item) => {
+
+    if (item.category == categories[i]) {
+
+      let icona = `<div>
+                      <i class="${item.family} ${item.prefix}${item.name}" style="color: ${colors[i]}"></i>
+                      <div class="title">${item.name}</div>
+                    </div>`;
+        $('.icons').append(icona);
+    }
+
+  });
+
+
+}
+
+
+// Milestone 2 senza ES6
 // var iconeFontawesome = document.getElementsByTagName("i");
 // for(var i = 0; i < iconeFontawesome.length; i++){
 //
